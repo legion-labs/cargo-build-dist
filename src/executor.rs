a4f51bfbbd1933cf2e2200402c96f43088be212f
Jalal El Mansouri <jalal.elmansouri@legionlabs.com>
2021-07-18 21:35:35 -0400

    Add some functionnality to bootstrap liem

--- src/executor.rs
+++ src/executor.rs
@@ -1,3 +1,40 @@
 //! So far nothing has been written to the file system
 //! the executor takes the result of the planner and runs it
 //! if --no-run is specified the executor early out.
+
+/// trying the use of template, easier than manipulating strings
+pub fn render() {
+    let tpl_name = "Dockerfile";
+    if let Ok(template) = tera::Template::new(
+        tpl_name,
+        None,
+        include_str!("templates/Dockerfile.template"),
+    ) {
+        let mut tera = tera::Tera::default();
+        tera.set_escape_fn(escape_docker);
+        tera.autoescape_on(vec!["Dockerfile"]);
+        tera.templates.insert(tpl_name.to_string(), template);
+
+        let mut context = tera::Context::new();
+        context.insert("base", "ubuntu:20.04");
+        context.insert("executable", "cargo-dockerize");
+
+        if let Ok(result) = tera.render(tpl_name, &context) {
+            println!("{}", result);
+        }
+    }
+}
+
+fn escape_docker(input: &str) -> String {
+    let mut output = String::with_capacity(input.len() * 2);
+    for c in input.chars() {
+        match c {
+            '\n' => output.push_str("\\"),
+            '\r' => output.push_str(""),
+            _ => output.push(c),
+        }
+    }
+
+    // Not using shrink_to_fit() on purpose
+    output
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::env;
+
+use clap::{App, AppSettings, Arg, SubCommand};
+
 fn main() -> Result<(), String> {
     let cargo = std::env::var("CARGO");
     if let Err(e) = &cargo {
@@ -6,7 +10,38 @@ fn main() -> Result<(), String> {
         return Err(format!("cargo not found: {}", e));
     }
     let cargo = cargo.unwrap();
+    
+    let args: Vec<_> = env::args_os().collect();
+    let matches = App::new("Carog Dockerize")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author("Legion Labs <devs@legionlabs.com>")
+        .about("Help managing Docker images containing cargo build artifacts")
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .subcommand(
+            SubCommand::with_name("build")
+                .about("build docker images")
+                .arg(
+                    Arg::with_name("debug")
+                        .short("d")
+                        .required(false)
+                        .help("print debug information verbosely"),
+                ),
+        )
+        .get_matches_from(&args[1..]);
+
     cargo_dockerize::Context::build(&cargo)?;
+
+    // You can handle information about subcommands by requesting their matches by name
+    // (as below), requesting just the name used, or both at the same time
+    if let Some(matches) = matches.subcommand_matches("build") {
+        if matches.is_present("debug") {
+            // do cargo build --debug
+            cargo_dockerize::render();
+        } else {
+            // do cargo build --release
+            cargo_dockerize::render();
+        }
+    }
 
     Ok(())
 }
